523a4dd60a98ca15f7e91aeff7449f157c53f82e
kaffudev <kaffudev@gmail.com>
2021-07-06 22:08:35 +0200

    add adapter - UserController

---
+++ adapter/controller/src/main/java/UserController.java
@@ -0,0 +1,42 @@
+import com.kf.example.usecase.CreateUser;
+import com.kf.example.usecase.FindUser;
+import com.kf.example.usecase.LoginUser;
+import model.UserWeb;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UserController {
+
+    private final CreateUser createUser;
+    private final FindUser findUser;
+    private final LoginUser loginUser;
+
+
+    public UserController(CreateUser createUser, FindUser findUser, LoginUser loginUser) {
+        this.createUser = createUser;
+        this.findUser = findUser;
+        this.loginUser = loginUser;
+    }
+
+    public UserWeb createUser(final UserWeb userWeb) {
+        var user = userWeb.toUser();
+        return UserWeb.toUserWeb(createUser.create(user));
+    }
+
+    public UserWeb loginUser(final String email, final String password) {
+        return UserWeb.toUserWeb(loginUser.login(email, password));
+    }
+
+    public UserWeb getUser(final String userId) {
+        return UserWeb.toUserWeb(findUser.findById(userId)
+                .orElseThrow(() -> new RuntimeException("user not found")));
+    }
+
+    public List<UserWeb> findAllUser() {
+        return findUser.findAllUsers()
+                .stream()
+                .map(UserWeb::toUserWeb)
+                .collect(Collectors.toList());
+    }
+}
---
+++ adapter/controller/src/main/java/model/UserWeb.java
@@ -0,0 +1,72 @@
+package model;
+
+import com.kf.example.domain.entity.User;
+
+public class UserWeb {
+    private String id;
+    private String email;
+    private String password;
+    private String lastName;
+    private String firstName;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public User toUser() {
+        return User.builder()
+                .withId(id)
+                .withPassword(password)
+                .withEmail(email)
+                .withFirstName(firstName)
+                .withLastName(lastName)
+                .build();
+    }
+
+    public static UserWeb toUserWeb(final User user) {
+        var userWeb = new UserWeb();
+        // do not map password
+        userWeb.setId(user.getId());
+        userWeb.setEmail(user.getEmail());
+        userWeb.setFirstName(user.getFirstName());
+        userWeb.setLastName(user.getLastName());
+        return userWeb;
+    }
+
+}
